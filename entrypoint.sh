fd60dbb41264abc2b31346489da7151085187b59
gautaz <11046232+gautaz@users.noreply.github.com>
2023-02-13 17:21:16 +0100

    fix database permissions issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,26 @@
 set -euf -o pipefail
 
 STATFILE=~/h2.status
-echo "STARTING" > "$STATFILE"
+echo "STARTING" | tee "$STATFILE"
 
 if [ -z "$H2_PASSWD" ]; then
 	echo "No database password specified, please define the H2_PASSWD environment variable." 2>&1
-	echo "FAILURE" > "$STATFILE"
+	echo "FAILURE" | tee "$STATFILE"
 	exit 10
 fi
 
 {
-	if /usr/bin/java -cp "/opt/h2database/h2.jar:/opt/h2database/dependency-check-core.jar" org.h2.tools.Shell -url "jdbc:h2:$H2_DATABASE" -user "$H2_USER" -password "$H2_INITPASSWD" -sql "ALTER USER $H2_USER SET PASSWORD '$H2_PASSWD';"; then
-		echo "STARTED" > "$STATFILE"
-	else
-		echo "FAILURE" > "$STATFILE"
-		echo "Unable to alter database password" 2>&1
-		killall java
-		exit 1
+	sleep 2
+	if ! /usr/bin/java -cp '/opt/h2database/h2.jar:/opt/h2database/dependency-check-core.jar' org.h2.tools.Shell -url "jdbc:h2:tcp://localhost/$H2_DATABASE" -user "$H2_USER" -password "$H2_PASSWD" -sql 'SELECT 1;' 2>&1 | head -n 1; then
+		echo "Initializing dependency check database"
+		if ! /usr/bin/java -cp '/opt/h2database/h2.jar:/opt/h2database/dependency-check-core.jar' org.h2.tools.RunScript -url "jdbc:h2:$H2_DATABASE" -user "$H2_USER" -password "$H2_PASSWD" -script /opt/h2database/initialize.sql; then
+			echo "FAILURE" | tee "$STATFILE"
+			killall java
+			exit 1
+		fi
 	fi
+
+	echo "STARTED" | tee "$STATFILE"
 } &
 
-exec /usr/bin/java -cp "/opt/h2database/h2.jar:/opt/h2database/dependency-check-core.jar" org.h2.tools.Server "$@"
+exec /usr/bin/java -cp '/opt/h2database/h2.jar:/opt/h2database/dependency-check-core.jar' org.h2.tools.Server "$@"
